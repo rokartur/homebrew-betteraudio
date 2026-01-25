b86762eca7b4321ef17d89cee77db29deca0bf27
rokartur <rokartur@icloud.com>
2026-01-25 05:37:32 +0100

    Add BetterAudio cask

---
+++ Casks/betteraudio.rb
@@ -0,0 +1,26 @@
+cask "betteraudio" do
+  version "26.4.1"
+  sha256 "a700b18020d091749c0e835659502f4c815e775eba430cab1292c80d6fa82519"
+  
+  url "https://github.com/rokartur/BetterAudio/releases/download/#{version}/BetterAudio-#{version}.zip"
+  name "BetterAudio"
+  desc "Master your Mac's audio with per-app volume control and precise device management"
+  homepage "https://github.com/rokartur/BetterAudio"
+  
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+  
+  app "BetterAudio.app"
+  
+  zap trash: [
+    '~/Library/Caches/pro.betteraudio.BetterAudio',
+    '~/Library/Caches/BetterAudio',
+    '~/Library/HTTPStorages/pro.betteraudio.BetterAudio',
+    '~/Library/HTTPStorages/BetterAudio',
+    '~/Library/Preferences/pro.betteraudio.BetterAudio.plist',
+    '~/Library/Preferences/BetterAudio.plist',
+    '~/Library/Application Support/BetterAudio'
+  ]
+end
