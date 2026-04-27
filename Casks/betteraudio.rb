9090e671d422f51cb81334dff1f92ca9b7e21b80
rokartur <rokartur@icloud.com>
2026-04-27 13:50:29 +0200

    Update BetterAudio download URL to use .dmg format

--- Casks/betteraudio.rb
+++ Casks/betteraudio.rb
@@ -2,7 +2,7 @@ cask "betteraudio" do
   version "26.5.6"
   sha256 "f0793117de3461113337da151300b489acfe25e151767ea856b213690ea5fa41"
   
-  url "https://github.com/rokartur/BetterAudio/releases/download/#{version}/BetterAudio-#{version}.zip"
+  url "https://github.com/rokartur/BetterAudio/releases/download/#{version}/BetterAudio-#{version}.dmg"
   name "BetterAudio"
   desc "Master your Mac's audio with per-app volume control and precise device management"
   homepage "https://github.com/rokartur/BetterAudio"
